da56cc3fce9a9cebed4869373134348258854d8c
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-04-05 18:04:36 -0400

    added list comperhension and generator exp

--- functional_programming/filter_func.py
+++ functional_programming/filter_func.py
@@ -25,7 +25,7 @@ pprint( tuple(filter( lambda x : x.field == 'physics' and x.nobel is True , scie
 """ Declaritive and only functions
     Can replace for in loop
 """
-print('##########################')
+print('########################## Filter with defined function')
 
 def nobel_filter(input):
     """ Return only records with nobel prize
@@ -33,3 +33,18 @@ def nobel_filter(input):
     return input.nobel is True
 
 pprint( tuple( filter(nobel_filter, scientists) ))
+
+
+# List Comprehensions
+print('########################## List Comprehensions')
+pprint ( tuple([ item for item in scientists if item.nobel is True ]) )
+
+# get rid of List Comprehensions
+print('########################## generator expression')
+pprint ( tuple( item for item in scientists if item.nobel is True) )
+""" Here we are not creating a list obj as intermidiate
+    A generator expressions defines an ad hoc iterator
+    that then produces values for you without first creating a list,
+    and then creating a tuple from that list, and then destroying the list again,
+    so it’s more memory efficient.
+"""
