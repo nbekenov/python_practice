21fab9015a6f6828048c0cdb8b4927e067d1bd3e
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-04-13 17:57:09 -0400

    added PyTest fixtures

--- testing_in_python/tests/test_stack.py
+++ testing_in_python/tests/test_stack.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 from ds.stack import Stack
+import pytest
+
+
+@pytest.fixtures
+def stack():
+    """ PyTest fixtures
+        a great way to provide a fixed baseline
+        upon which tests can reliably and repeatedly executed.
+    """
+    return Stack()
 
 def test_constructor():
+    """
+        Test Stack class constructor
+    """
     s = Stack()
     assert  isinstance(s, Stack)
     assert len(s) == 0
+
+def test_push(stack):
+    stack.push(3)
+    assert len(stack) == 1
