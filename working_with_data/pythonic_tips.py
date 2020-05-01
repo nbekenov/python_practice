6c2d222ac47e4b36f6bc773fcc08d653f194d700
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-04-30 20:05:14 -0400

    add fizbuzz function

--- working_with_data/pythonic_tips.py
+++ working_with_data/pythonic_tips.py
@@ -1,4 +1,16 @@
 # use enumerate
 my_list = [45, 22, 14, 65, 97, 72]
+
+
+def fizz_buzz(numbers):
+    for item, value in enumerate(numbers):
+        if value % 3 == 0 and value % 5 == 0:
+            numbers[item] = 'fizzbuzz'
+        elif value % 3 == 0:
+            numbers[item] = 'fizz'
+        elif value % 5 == 0:
+            numbers[item] = 'buzz'
+
+fizz_buzz(my_list)
 for item, value in enumerate(my_list):
     print(f' index = {item}, value = {value}')
